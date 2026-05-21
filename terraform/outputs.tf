51c3ffbbb17fbd3f8c81129ba0801baf99a9f7b9
rsantiag085 <robsonsanoliveira@gmail.com>
2026-05-21 15:51:44 -0300

    chore: remove .agents, skills-lock.json e terraform do repositório

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "The public IP address of the FocusTrack AI server."
-  value       = aws_instance.focustrack_server.public_ip
-}
-
-output "ssh_command" {
-  description = "SSH command to connect to the instance."
-  value       = "ssh -i \"~/.ssh/focus-ssh\" ec2-user@${aws_instance.focustrack_server.public_ip}"
-}
